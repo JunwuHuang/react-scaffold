a732662bb00a912b73cb52a4545270224e59e6f8
JunwuHuang <978162622@qq.com>
2021-12-01 21:44:12 +0800

    fix: 修复菜单无法根据路由响应的问题

--- src/layouts/AppSider.tsx
+++ src/layouts/AppSider.tsx
@@ -1,9 +1,25 @@
-import { Link } from "react-router-dom"
+import { ReactElement, useState, useEffect } from "react"
+import { useLocation, Link } from "react-router-dom"
 import { Layout, Menu } from "antd"
-import { ReactElement } from "react"
 import { IRouteObject, routes } from "@/routes"
 
 const AppSider = () => {
+  const { pathname } = useLocation()
+  const [openKeys, setOpenKeys] = useState<string[]>([])
+
+  useEffect(() => {
+    const pathSnippets = pathname.split("/").filter((i) => i)
+    setOpenKeys(
+      pathSnippets.map((item, index) => {
+        let result = `/${item}/`
+        for (let i = index; i > 0; i -= 1) {
+          result = `/${pathSnippets[i]}${result}`
+        }
+        return result
+      })
+    )
+  }, [pathname])
+
   const generateMenus = (
     _routes: IRouteObject[],
     prefix = ""
@@ -11,26 +27,33 @@ const AppSider = () => {
     _routes.reduce((result: ReactElement[], route) => {
       const path = `${prefix}/${route.path ?? ""}`
       const title = route.meta?.title ?? path
-      if (route.children) {
-        return result.concat(
-          <Menu.SubMenu key={path} title={title}>
-            <Menu.Item key={`${path}/`}>
-              <Link to={path}>{title}</Link>
-            </Menu.Item>
-            {generateMenus(route.children ?? [], path)}
-          </Menu.SubMenu>
-        )
-      }
-      return result.concat(
+      const item = (
         <Menu.Item key={path}>
           <Link to={path}>{title}</Link>
         </Menu.Item>
       )
+      if (route.children) {
+        return result.concat(
+          <Menu.SubMenu key={`${path}/`} title={title}>
+            {item}
+            {generateMenus(route.children ?? [], path)}
+          </Menu.SubMenu>
+        )
+      }
+      return result.concat(item)
     }, [])
 
   return (
     <Layout.Sider>
-      <Menu theme="dark" mode="inline" defaultSelectedKeys={["index"]}>
+      <Menu
+        theme="dark"
+        mode="inline"
+        openKeys={openKeys}
+        selectedKeys={[pathname]}
+        onOpenChange={(_openKeys: string[]) => {
+          setOpenKeys(_openKeys)
+        }}
+      >
         {generateMenus(routes)}
       </Menu>
     </Layout.Sider>
